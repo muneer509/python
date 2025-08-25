2e55f25144cd2f6a871ff7ac1cd0c42ceec2f384
Muneer Shaik <mshaik4@berkadia.com>
2025-08-25 10:32:15 +0530

    practice

--- dictonary_datatype.py
+++ dictonary_datatype.py
@@ -1,3 +1,5 @@
 # Its a key value pair based data types
-d={'Name':'Muneer'}
-print(d)
+# d={'Name':'Muneer'}
+# print(d)
+d={'k':123,(1,2,3,4):123,(1,2,3,4,5):list(str(123))}
+print(d[(1,2,3,4,5)])
