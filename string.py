4450be21e95e6f37701276647f93d388c09104b7
Muneer Shaik <mshaik4@berkadia.com>
2025-08-25 09:31:17 +0530

    practice

--- string.py
+++ string.py
@@ -1,12 +1,13 @@
-# l =[10, 20, 30, 40, 50, "abcd", True]
+# # l =[10, 20, 30, 40, 50, "abcd", True]
+# # # print(l)
+# # # print(l[2])
+# # # l.extend[6, 7, 8, 9, False]
+# # # print(l)
+# l =[2, 3, 1, 5, 4, 8]
+# # l.sort()
 # # print(l)
-# # print(l[2])
-# # l.extend[6, 7, 8, 9, False]
-# # print(l)
-l =[2, 3, 1, 5, 4, 8]
-# l.sort()
-# print(l)
 
-l1 = sorted(l)
-print(l1)
-print(l)
+# l1 = sorted(l)
+# print(l1)
+# print(l)
+# txt = "Hello,"
