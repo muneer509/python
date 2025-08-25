e5a20c14705029818f8c96da0d7ded5d05d765a2
Muneer Shaik <mshaik4@berkadia.com>
2025-08-25 09:51:26 +0530

    practice

--- tuple_datatype.py
+++ tuple_datatype.py
@@ -2,5 +2,8 @@ t =(1, 2, 3, 4, True, False, [4, 5, 6, 7])
 # # print(type(t))
 # t1 = t[-1]
 # print(t1)
-t[0]=200
-print(t)
+# t[0]=200
+# print(t)
+
+# tuple is an immutable datatype
+print(dir(t))
