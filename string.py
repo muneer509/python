becf1233ded9f8ee1d0cf6253dabf84a81c496dc
Muneer Shaik <mshaik4@berkadia.com>
2025-08-25 09:24:24 +0530

    practice

--- string.py
+++ string.py
@@ -1,5 +1,7 @@
-l =[10, 20, 30, 40, 50, "abcd", True]
-# print(l)
-# print(l[2])
-l.extend[6, 7, 8, 9, False]
+# l =[10, 20, 30, 40, 50, "abcd", True]
+# # print(l)
+# # print(l[2])
+# # l.extend[6, 7, 8, 9, False]
+# # print(l)
+l =[2, 3, 1 'a']
 print(l)
